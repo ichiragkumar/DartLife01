ecd2188cd8c7dfa3edc07e2e4a0a243db8a57c2f
chirag kumar <74717424+chirag270@users.noreply.github.com>
2022-05-21 00:06:38 +0530

    Create 01.dart
    first program of dart

---
+++ 01.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  var greetings = Greet();
+  print("$greetings are brother");
+  
+}
+
+
+String Greet(){
+  
+  var n1 ="Ram";
+  var n2=" and Shyam";
+  var n3 = n1+n2;
+  return n3;
+  
+}
